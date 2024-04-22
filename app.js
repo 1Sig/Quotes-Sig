c53738752c23820cd41d9beb83823d2f8c430d51
1Sig <Sigurdowre@gmail.com>
2024-04-22 14:19:14 +0200

    jaaaa

--- app.js
+++ app.js
@@ -1,62 +1,42 @@
-// Importer Express og andre nødvendige moduler
+// Import necessary modules
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose'); 
 const path = require('path');
+const User = require('./models/user');
+const routes = require('./routes');
 
-// Importer rutefilen for autentisering
-const authRoutes = require('./routes/auth');
-
-// Opprett en instans av Express-appen
+// Create an instance of the Express app
 const app = express();
 
-// Konfigurer session-middleware
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/Qoutes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+// Set up session middleware
 app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: false
 }));
 
-// Initialiser Passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Middleware for å gjøre brukeren tilgjengelig i malene
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
-
 // Sett opp visningsmotoren til EJS
 app.set('view engine', 'ejs');
 
-// Angi katalogen for statiske filer (CSS, JS, bilder)
-app.use(express.static(path.join(__dirname, 'public')));
+// Set up JSON parsing for request bodies
+app.use(express.json());
 
-// Sett opp ruter for autentisering
-app.use('/', authRoutes);
+// Set up routes
+app.use('/', routes);
 
-// Sett opp rute for hjemmesiden
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('css', (req, res) => {
-    res.render('styles.css')
-})
-
-// Sett opp rute for login-siden
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-// Sett opp rute for register-siden
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-// Start serveren
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
